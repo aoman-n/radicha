77b4e33af913dec4fd093fb95a5f6137046a22dc
aoba <ipbdipbd00@gmail.com>
2018-12-20 00:48:49 +0900

    client: Use lib redux-actions

--- client/src/js/actions/index.js
+++ client/src/js/actions/index.js
@@ -1,30 +1,15 @@
-// message
+import { createAction } from 'redux-actions';
+
+// actions
 export const SENT_MESSAGE = 'SEND_MESSAGE';
 export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
 export const CREATE_CONNECTION = 'CREATE_CONNECTION';
 export const SET_SOCKET = 'SET_SOCKET';
-// viewContens
 export const TOGLLE_ROOMBAR = 'TOGLLE_ROOMBAR';
 
-export const sendMessage = text => ({
-  type: SENT_MESSAGE,
-  payload: text,
-});
-
-export const receiveMessage = text => ({
-  type: RECEIVE_MESSAGE,
-  payload: text,
-});
-
-export const createConnection = () => ({
-  type: CREATE_CONNECTION,
-});
-
-export const setSocket = socket => ({
-  type: SET_SOCKET,
-  payload: socket,
-});
-
-export const toggleRoombar = () => ({
-  type: TOGLLE_ROOMBAR,
-});
+// actionCreators
+export const sendMessage = createAction(SENT_MESSAGE);
+export const receiveMessage = createAction(RECEIVE_MESSAGE);
+export const createConnection = createAction(CREATE_CONNECTION);
+export const setSocket = createAction(SET_SOCKET);
+export const toggleRoombar = createAction(TOGLLE_ROOMBAR);
