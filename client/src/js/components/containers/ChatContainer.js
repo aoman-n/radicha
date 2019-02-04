c12e09a87a61b680d9ed292c01b1f59d949ab91f
aoba <ipbdipbd00@gmail.com>
2019-02-04 23:54:16 +0900

    client: Redirect when a user not logged in join the room.

--- client/src/js/components/containers/ChatContainer.js
+++ client/src/js/components/containers/ChatContainer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { compose, pure, lifecycle } from 'recompose';
+import { withRouter } from 'react-router-dom';
 import Index from '../presentators/MainContents';
-import { sendMessage, joinRoom, removeRoom } from '../../actions';
+import {
+  sendMessage,
+  joinRoom,
+  removeRoom,
+  showLoginModal,
+} from '../../actions';
 
 const enhancer = compose(
   connect(
@@ -14,19 +20,25 @@ const enhancer = compose(
       sendMessage,
       joinRoom,
       removeRoom,
+      showLoginModal,
     },
   ),
   lifecycle({
     componentWillMount() {
       const { joinRoom, match, app } = this.props;
-      // 直接URLを叩かれたときの暫定的な対応、socketがsetされるまで待つ
-      // @TODO: REFACTOR
-      if (!app.socket) {
-        setTimeout(() => {
-          joinRoom(match.params.roomId);
-        }, 2000);
+      if (app.username === null || app.username === '') {
+        this.props.showLoginModal();
+        this.props.history.push('/');
+      } else {
+        // 直接URLを叩かれたときの暫定的な対応、socketがsetされるまで待つ
+        // @TODO: REFACTOR
+        if (!app.socket) {
+          setTimeout(() => {
+            joinRoom(match.params.roomId);
+          }, 2000);
+        }
+        joinRoom(match.params.roomId);
       }
-      joinRoom(match.params.roomId);
     },
     componentWillReceiveProps(nextProps) {
       if (nextProps.location !== this.props.location) {
@@ -38,6 +50,8 @@ const enhancer = compose(
   pure,
 );
 
-export default enhancer(({ app, chatRoom, sendMessage, removeRoom }) => (
+const ChatContainer = enhancer(({ app, chatRoom, sendMessage, removeRoom }) => (
   <Index {...{ app, chatRoom, sendMessage, removeRoom }} />
 ));
+
+export default withRouter(ChatContainer);
