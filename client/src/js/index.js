723bf6348e164cc8fbbd950a53f872b02f692f44
aoba <ipbdipbd00@gmail.com>
2019-02-12 23:27:52 +0900

    client: Remove unnecessary line

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -4,28 +4,8 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Router } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import '../css/reset.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import {
-  faCheckSquare,
-  faCoffee,
-  faBars,
-  faVolumeUp,
-  faUser,
-  faPlusSquare,
-} from '@fortawesome/free-solid-svg-icons';
-import Routes from './Routes';
+import Routes from './routes';
 import createStore from './store';
-
-library.add(
-  fab,
-  faCheckSquare,
-  faCoffee,
-  faBars,
-  faVolumeUp,
-  faUser,
-  faPlusSquare,
-);
 
 const history = createHistory();
 const store = createStore(history);
--- client/src/js/routes.js
+++ client/src/js/routes.js
@@ -2,19 +2,9 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import Home from './components/containers/HomeContainer';
-import AuthContainer from './components/containers/AuthContainer';
-import Login from './Login';
 
 const Routes = () => (
   <div>
-    {/* <Switch>
-      <Route exact path="/login" component={Login} />
-        <AuthContainer>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </AuthContainer>
-    </Switch> */}
     <Switch>
       <Route path="/" component={Home} />
     </Switch>
