1599f1d96ca14e89a41ab6a231d4c2faa104dafb
aoba <ipbdipbd00@gmail.com>
2019-02-16 16:15:04 +0900

    server: Move routes file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Mongo = require('./mongo');
+const Mongo = require('../mongo');
 
 router.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
